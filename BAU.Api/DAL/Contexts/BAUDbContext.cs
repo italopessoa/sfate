6602b3a4cfa4942443dfc82288ecbf9f6bfb9f34
Italo Pessoa <italo.pessoa@hotmail.com>
2017-12-12 18:03:52 -0300

    fix missing DAL folder

---
+++ BAU.Api/DAL/Contexts/BAUDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BAU.Api.DAL.Contexts
+{
+    /// <summary>
+    /// Database context
+    /// </summary>
+    public class BAUDbContext : DbContext
+    {
+        
+    }
+}
