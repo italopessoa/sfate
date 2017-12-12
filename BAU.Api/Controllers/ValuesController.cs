95a488d85df5fdc2ead92c82e4dbb786902e4213
Italo Pessoa <italo.pessoa@hotmail.com>
2017-12-11 23:10:24 -0300

    add initial configuration

---
+++ BAU.Api/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BAU.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] 
+            {
+                Environment.GetEnvironmentVariable("DB_SERVER"),
+                Environment.GetEnvironmentVariable("DB_CATALOG"),
+                Environment.GetEnvironmentVariable("DB_USER"),
+                Environment.GetEnvironmentVariable("DB_PASSWORD")
+            };
+        }
+    }
+}
