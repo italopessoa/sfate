97bfdff2d7b8aff66e976ff6cc8eaf0df885698b
Italo Pessoa <italo.pessoa@hotmail.com>
2017-12-13 08:18:17 -0300

    enhancing documentation and tests

--- BAU.Test/DAL/Repositories/ShiftRepositoryTest.cs
+++ BAU.Test/DAL/Repositories/ShiftRepositoryTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using BAU.Api.DAL.Contexts;
+using BAU.Api.DAL.Models;
 using BAU.Api.DAL.Repositories;
 using BAU.Api.DAL.Repositories.Interface;
 using Xunit;
@@ -14,104 +16,155 @@ namespace BAU.Test.DAL.Repositories
         [Fact]
         public void GetAvailableEngineers_2Engineers_NoEngineers()
         {
-            _contextNames.Add("GetAvailableEngineers_2Engineers_NoEngineers");
-            var options = DbContextUtils.GetContextOptions("GetAvailableEngineers_2Engineers_NoEngineers");
+            string contextName = "GetAvailableEngineers_2Engineers_NoEngineers";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
+
             // fake data
-            using (var context = new BAUDbContext(options))
+            using (BAUDbContext context = new BAUDbContext(options))
             {
-                IShiftRepository repository = new ShiftRepository(context);
-                repository.GetAvailableEngineers(1);
+                var engineer1 = new Engineer { Name = "Engineer 1" };
+                var engineer2 = new Engineer { Name = "Engineer 2" };
+                var engineersList = new List<Engineer> 
+                {
+                    engineer1,
+                    engineer2,
+                };
+
+                context.Engineers.AddRange(engineersList);
+                context.SaveChanges();
             }
 
             // test
             using (var context = new BAUDbContext(options))
             {
-                IShiftRepository repository = new ShiftRepository(context);
-                repository.GetAvailableEngineers(1);
-            }
-
-            // clear
-            using (var context = new BAUDbContext(options))
-            {
-                context.Database.EnsureDeleted();
             }
         }
 
-        // [Fact]
-        // public void GetAvailableEngineers_2Engineers_OnlyOne()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+         [Fact]
+        public void GetEngineersAvailableOn_All_NoEngineers()
+        {
+            string contextName = "GetAvailableEngineersAt_2Engineers_NoEngineers";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
 
-        // [Fact]
-        // public void GetAvailableEngineers_2Engineers_Success()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+                var engineer1 = new Engineer { Name = "Engineer 1" };
+                var engineer2 = new Engineer { Name = "Engineer 2" };
+                var engineersList = new List<Engineer> 
+                {
+                    engineer1,
+                    engineer2,
+                };
 
-        // [Fact]
-        // public void SetEngineerShift_Success()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+                context.Engineers.AddRange(engineersList);
+                context.SaveChanges();
+            }
 
-        // [Fact]
-        // public void SetEngineerShift_ConsecutiveDays_Error()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
 
-        // [Fact]
-        // public void SetEngineerShift_WholeDayInPeriod_Error()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
 
-        // [Fact]
-        // public void SetEngineerShift_SameDay_Error()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+        [Fact]
+        public void SetEngineerShift_Success()
+        {
+            string contextName = "SetEngineerShift_Success";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
 
-        // [Fact]
-        // public void GetEngineerShifts_Success()
-        // {
-        //     using (var context = DbContextUtils.GetContext())
-        //     {
-        //         IShiftRepository repository = new ShiftRepository(context);
-        //         repository.GetAvailableEngineers(1);
-        //     }
-        // }
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+            }
+
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
+
+        [Fact]
+        public void SetEngineerShift_ConsecutiveDays_Error()
+        {
+            string contextName = "SetEngineerShift_ConsecutiveDays_Error";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
+
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+            }
+
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
+
+        [Fact]
+        public void SetEngineerShift_WholeDayInPeriod_Error()
+        {
+            string contextName = "SetEngineerShift_WholeDayInPeriod_Error";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
+
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+            }
+
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
+
+        [Fact]
+        public void SetEngineerShift_SameDay_Error()
+        {
+            string contextName = "SetEngineerShift_SameDay_Error";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
+
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+            }
+
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
+
+        [Fact]
+        public void GetEngineerShifts_Success()
+        {
+            string contextName = "GetAvailableEngineers_2Engineers_NoEngineers";
+            _contextNames.Add(contextName);
+            var options = DbContextUtils.GetContextOptions(contextName);
+
+            // fake data
+            using (BAUDbContext context = new BAUDbContext(options))
+            {
+            }
+
+            // test
+            using (var context = new BAUDbContext(options))
+            {
+            }
+        }
 
         public void Dispose()
         {
-            foreach (string name in _contextNames)
+            foreach (string contextName in _contextNames)
             {
-                var options = DbContextUtils.GetContextOptions("GetAvailableEngineers_2Engineers_NoEngineers");
+                var options = DbContextUtils.GetContextOptions(contextName);
                 using (var context = new BAUDbContext(options))
                 {
                     context.Database.EnsureDeleted();
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ used to manage engineers turns find engineers save turns
 used to configure how much time takes each turn and the maximum amount of time an engineer can have in his N weeks period. To avoid creating a new table with only two values that will barely change, I decided to use environment variables to store them.
 
 ### CI/CD 
-    AppVeyor and Windows Azure
+AppVeyor and Windows Azure
 
 - DAL is just a folder not a class library
 
@@ -15,3 +15,8 @@ used to configure how much time takes each turn and the maximum amount of time a
 
 ### AuthController
 - use JWT to authenticate user, even though theres is not solid user management implementation, token based authentication works just fine, and will make easier integrate with other clients.
+
+
+### Architecture
+
+there is only two layer on the project, DAL (Data Access Layer) and Controllers. As the project still in development and does not have many features create more layers like a Service or Facade could lead to needless complexity
