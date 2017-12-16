b6f2ff954efc99fd844fcdca8137e5b73e1056c7
Italo Pessoa <italo.pessoa@hotmail.com>
2017-12-15 23:24:02 -0300

    document filter methods

--- BAU.Api/DAL/Repositories/ShiftRepository.cs
+++ BAU.Api/DAL/Repositories/ShiftRepository.cs
@@ -35,27 +35,10 @@ namespace BAU.Api.DAL.Repositories
             var lastWeek_Monday = shiftDate.PreviousDayOfWeek(DayOfWeek.Monday, 1);
             var endOfWeek = shiftDate.NextDayOfWeek(DayOfWeek.Friday);
 
-            IQueryable<EngineerShift> engineerShifts = FindEngineersShiftsByPeriod(lastWeek_Monday, endOfWeek);
+            IQueryable<EngineerShift> engineerShifts = FilterEngineersShiftsByPeriod(lastWeek_Monday, endOfWeek);
+            engineerShifts = FilterEngineerShiftsByMaxShiftHours(engineerShifts);
             engineerShifts = FilterEngineerShiftsByConsecutiveShiftDays(engineerShifts, shiftDate);
-
-            return FilterEngineerShiftsByMaxShiftHours(engineerShifts).ToList();
-        }
-        private IQueryable<EngineerShift> FindEngineersShiftsByPeriod(DateTime from, DateTime to)
-        {
-            return _context.EngineersShifts.Include(es => es.Engineer)
-                .Where(es => from.Date <= es.Date && es.Date <= to.Date);
-        }
-        private IQueryable<Engineer> FilterEngineerShiftsByMaxShiftHours(IQueryable<EngineerShift> engineerShifts)
-        {
-            return engineerShifts.GroupBy(es => new { es.Engineer, es.Duration })
-                .Where(es => es.Sum(s => s.Duration) < 8)
-                .Select(es => es.Key.Engineer);
-        }
-
-        private IQueryable<EngineerShift> FilterEngineerShiftsByConsecutiveShiftDays(IQueryable<EngineerShift> engineerShifts, DateTime date)
-        {
-            DateTime previous = date.DayOfWeek == DayOfWeek.Monday ? date.PreviousDayOfWeek(DayOfWeek.Friday) : date.PreviousBusinessDay();
-            return engineerShifts.Where(shift => shift.Date < previous || shift.Date > date.NextBusinessDay());
+            return engineerShifts.Select(x => x.Engineer).ToList();
         }
 
         public void ScheduleEngineerShift(int engineerId, DateTime date, int duration)
@@ -71,5 +54,47 @@ namespace BAU.Api.DAL.Repositories
         {
             throw new NotImplementedException();
         }
+
+        #region filters
+
+        /// <summary>
+        /// Filter EngineersShifts by period
+        /// </summary>
+        /// <param name="from">Start date</param>
+        /// <param name="to">End date</param>
+        /// <returns>EngineerShifts between from-end period</returns>
+        private IQueryable<EngineerShift> FilterEngineersShiftsByPeriod(DateTime from, DateTime to)
+        {
+            return _context.EngineersShifts.Include(es => es.Engineer)
+                .Where(es => from.Date <= es.Date && es.Date <= to.Date);
+        }
+
+        /// <summary>
+        /// Filter EngineerShifts by max shift hours
+        /// </summary>
+        /// <param name="engineerShifts">EngineerShifts</param>
+        /// <returns>EngineerShifts with sum of all shifts less than the max time allowed</returns>
+        private IQueryable<EngineerShift> FilterEngineerShiftsByMaxShiftHours(IQueryable<EngineerShift> engineerShifts)
+        {
+            var engineersWithMaxShiftHours = engineerShifts.GroupBy(es => new { es.Engineer.Id, es.Duration })
+                .Where(es => es.Sum(s => s.Duration) < 8)
+                .Select(es => es.Key.Id).ToList();
+            return engineerShifts.Where(e => engineersWithMaxShiftHours.Contains(e.EngineerId));
+        }
+
+        /// <summary>
+        /// Filter EngineerShifts by consecutive shift days
+        /// </summary>
+        /// <param name="engineerShifts">EngineerShifts</param>
+        /// <param name="date">Target date</param>
+        /// <returns>EngineerShifts that are on non-consecutive days based on target date</returns>
+        private IQueryable<EngineerShift> FilterEngineerShiftsByConsecutiveShiftDays(IQueryable<EngineerShift> engineerShifts, DateTime date)
+        {
+            DateTime previous = date.DayOfWeek == DayOfWeek.Monday ? date.PreviousDayOfWeek(DayOfWeek.Friday) : date.PreviousBusinessDay();
+            DateTime next = date.DayOfWeek == DayOfWeek.Friday ? date.NextDayOfWeek(DayOfWeek.Monday) : date.NextBusinessDay();
+            return engineerShifts.Where(shift => shift.Date < previous || shift.Date > next);
+        }
+
+        #endregion
     }
 }
