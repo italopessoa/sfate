5c6f16675d79a39e53620df83b3cd99d1617e6fe
Italo Pessoa <italo.pessoa@hotmail.com>
2017-12-17 12:33:28 -0300

    implement equals on resource classes

--- BAU.Api/Models/EngineerModel.cs
+++ BAU.Api/Models/EngineerModel.cs
@@ -4,5 +4,36 @@ namespace BAU.Api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            if (this.Name != (obj as EngineerModel).Name
+            || (this.Id != (obj as EngineerModel).Id))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + Id.GetHashCode();
+                if (!string.IsNullOrEmpty(Name))
+                {
+                    hash = hash * 23 + Name.GetHashCode();
+                }
+                return hash;
+            }
+        }
     }
 }
--- BAU.Api/Models/EngineerShiftModel.cs
+++ BAU.Api/Models/EngineerShiftModel.cs
@@ -7,5 +7,39 @@ namespace BAU.Api.Models
         public EngineerModel Engineer { get; set; }
         public DateTime Date { get; set; }
         public byte Duration { get; set; }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            if (this.Date != (obj as EngineerShiftModel).Date
+           || (this.Duration != (obj as EngineerShiftModel).Duration)
+           || (!this.Engineer.Equals((obj as EngineerShiftModel).Engineer)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + Date.GetHashCode();
+                hash = hash * 23 + Duration.GetHashCode();
+                if (Engineer != null)
+                {
+                    hash = hash * 23 + Engineer.GetHashCode();
+                }
+                return hash;
+            }
+        }
     }
 }
